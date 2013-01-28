84b9980d21864205e4ca34f25b9b51a58ac857e0
OscardR <oscar.gomez@uji.es>
2013-01-28 01:20:20 +0100

    Bookmarks editables y borrables. CSS decente. TODO: AJAX y mejorar tags.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,21 @@
-// when the DOM is ready:
 $(document).ready(function() {
-	var time = 500;
-	// find the div.fade elements and hook the hover event
-	$('div.delete').hover(function() {
-		// on hovering over, find the element we want to fade *up*
-		var fade = $('> div', this);
-
-		// if the element is currently being animated (to a fadeOut)...
-		if (fade.is(':animated')) {
-			// ...take it's current opacity back up to 1
-			fade.stop().fadeTo(time / 4, 1);
-		} else {
-			// fade in quickly
-			fade.fadeIn(time / 4);
-		}
+	// set up hover panels
+	// although this can be done without JavaScript, we've attached
+	// these events
+	// because it causes the hover to be triggered when the element is
+	// tapped on a touch device
+	$('.hover').hover(function() {
+		$(this).addClass('flip');
 	}, function() {
-		// on hovering out, fade the element out
-		var fade = $('> div', this);
-		if (fade.is(':animated')) {
-			fade.stop().fadeTo(time, 0);
-		} else {
-			// fade away slowly
-			fade.fadeOut(time);
-		}
+		$(this).removeClass('flip');
+	});
+
+	// set up click/tap panels
+	$('.click').click(function(e) {
+		if($(this).hasClass('cancelar'))
+			$(this).parent().parent().parent().toggleClass('flip');
+		else
+			$(this).parent().parent().toggleClass('flip');
+		e.preventDefault();
 	});
 });
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from google.appengine.ext import webapp
 from google.appengine.ext import db
 from google.appengine.api import users
@@ -25,16 +26,19 @@ class MainPage(webapp.RequestHandler):
         if user:
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
+            url_img = 'logout'
         else:
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
+            url_img = 'login'
 
         template_values = {
             'bookmarks' : bookmarks,
             'url' : url,
             'url_linktext' : url_linktext,
             'user' : user,
-            'allowed' : allowed
+            'allowed' : allowed,
+            'url_img' : url_img
         }
 
         template = jinja_environment.get_template('index.html')
@@ -55,12 +59,36 @@ class Submit(webapp.RequestHandler):
         bookmark.put()
         self.redirect('/')
 
+class Edit(webapp.RequestHandler):
+    def post(self):
+        bookmark = Bookmark()
+        user = users.get_current_user()
+        if user != None:
+            bookmark.user = users.get_current_user().email()
+        else:
+            bookmark.user = None
+        
+        # Borrar el bookmark existente
+        delete_bookmark(user, self.request.get('old_name'))
+        
+        # Añadir el bookmark cambiado
+        bookmark.name = self.request.get('name')
+        bookmark.url = self.request.get('url')
+        bookmark.icon = self.request.get('icon')
+        bookmark.rating = self.request.get('rating')
+        bookmark.tags = self.request.get('tags')
+        bookmark.put()
+        self.redirect('/')
+
 class Delete(webapp.RequestHandler):
     def get(self):
         user = users.get_current_user()
         name = self.request.get('b')
-        bookmark = Bookmark.gql('WHERE name = :1 AND user = :2', name, user.email() if user != None else None)
-        db.delete(bookmark)
+        delete_bookmark(user, name)
         self.redirect('/')
 
-app = webapp.WSGIApplication([('/', MainPage), ('/submit', Submit), ('/delete', Delete)], debug=True)
+def delete_bookmark(user, name):
+    bookmark = Bookmark.gql('WHERE name = :1 AND user = :2', name, user.email() if user != None else None)
+    db.delete(bookmark)
+
+app = webapp.WSGIApplication([('/', MainPage), ('/submit', Submit), ('/delete', Delete), ('/edit', Edit)], debug=True)
