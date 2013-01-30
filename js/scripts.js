d53303f92622bb6792cbc763f2c40d2d63505b6d
OscardR <oscar.gomez@uji.es>
2013-01-30 19:29:11 +0100

    Styles updated Star rating system updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
 	// set up hover panels
 	// although this can be done without JavaScript, we've attached
-	// these events
-	// because it causes the hover to be triggered when the element is
-	// tapped on a touch device
+	// these events because it causes the hover to be triggered
+	// when the element is tapped on a touch device
 	$('.hover').hover(function() {
 		$(this).addClass('flip');
 	}, function() {
@@ -12,10 +11,34 @@ $(document).ready(function() {
 
 	// set up click/tap panels
 	$('.click').click(function(e) {
-		if($(this).hasClass('cancelar'))
+		if ($(this).hasClass('cancelar'))
 			$(this).parent().parent().parent().toggleClass('flip');
 		else
 			$(this).parent().parent().toggleClass('flip');
+		e.preventDefault();
+	});
+
+	// Star rating system
+	$('[id^=star]').click(function(e) {
+		var input = $('[name=rating]');
+		var old_rating = input.val();
+		var rating = parseInt($(this).attr('id').charAt(4));
+
+		for ( var i = 1; i <= rating; i++) {
+			var star = $('#star' + i);
+			if (rating == old_rating)
+				star.addClass('no_star');
+			else
+				star.removeClass('no_star');
+		}
+		for ( var i = rating + 1; i <= 5; i++)
+			$('#star' + i).addClass('no_star');
+
+		if (rating == old_rating)
+			rating = 0;
+
+		input.val(rating);
+		$('#r' + rating).prop('checked', 'checked');
 		e.preventDefault();
 	});
 });
